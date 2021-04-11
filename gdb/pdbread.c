64c2374c19e6883fd7e2ca045c9c5313ef55120d
Mark Harmstone <mark@harmstone.com>
2021-04-11 19:58:14 +0100

    add pdb_symfile_init and pdb_symfile_offsets

--- gdb/pdbread.c
+++ gdb/pdbread.c
@@ -1,19 +1,32 @@
 #include "defs.h"
 #include "symfile.h"
 
+static void
+pdb_symfile_init (struct objfile *objfile)
+{
+  // FIXME
+}
+
+static void
+pdb_symfile_offsets (struct objfile *objfile,
+		       const section_addr_info &addrs)
+{
+  // FIXME
+}
+
 static const struct sym_fns pdb_sym_fns =
 {
-  NULL,		/* sym_new_init */
-  NULL,		/* sym_init */
-  NULL,		/* sym_read */
-  NULL,		/* sym_read_psymbols */
-  NULL,		/* sym_finish */
-  NULL,		/* sym_offsets */
-  NULL,		/* sym_segments */
-  NULL,		/* sym_read_linetable */
-  NULL,		/* sym_relocate */
-  NULL,		/* sym_probe_fns */
-  NULL		/* qf */
+  NULL,			/* sym_new_init */
+  pdb_symfile_init,
+  NULL,			/* sym_read */
+  NULL,			/* sym_read_psymbols */
+  NULL,			/* sym_finish */
+  pdb_symfile_offsets,
+  NULL,			/* sym_segments */
+  NULL,			/* sym_read_linetable */
+  NULL,			/* sym_relocate */
+  NULL,			/* sym_probe_fns */
+  NULL			/* qf */
 };
 
 void _initialize_pdbread ();
