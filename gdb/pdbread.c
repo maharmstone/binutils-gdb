3f771ac73c82cbebb23d4f0a69f2010b12f6adc9
Mark Harmstone <mark@harmstone.com>
2021-04-11 20:06:43 +0100

    minimal example

--- gdb/pdbread.c
+++ gdb/pdbread.c
@@ -1,5 +1,6 @@
 #include "defs.h"
 #include "symfile.h"
+#include "psymtab.h"
 
 static void
 pdb_symfile_init (struct objfile *objfile)
@@ -9,7 +10,15 @@ pdb_symfile_init (struct objfile *objfile)
 
 static void
 pdb_symfile_offsets (struct objfile *objfile,
-		       const section_addr_info &addrs)
+		     const section_addr_info &addrs)
+{
+  // FIXME?
+
+  default_symfile_offsets (objfile, addrs);
+}
+
+static void
+pdb_symfile_read (struct objfile *objfile, symfile_add_flags symfile_flags)
 {
   // FIXME
 }
@@ -18,15 +27,15 @@ static const struct sym_fns pdb_sym_fns =
 {
   NULL,			/* sym_new_init */
   pdb_symfile_init,
-  NULL,			/* sym_read */
+  pdb_symfile_read,
   NULL,			/* sym_read_psymbols */
   NULL,			/* sym_finish */
   pdb_symfile_offsets,
-  NULL,			/* sym_segments */
+  default_symfile_segments,
   NULL,			/* sym_read_linetable */
   NULL,			/* sym_relocate */
   NULL,			/* sym_probe_fns */
-  NULL			/* qf */
+  &psym_functions
 };
 
 void _initialize_pdbread ();
