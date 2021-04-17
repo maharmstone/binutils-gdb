4aad666df0e761ed5b7dfc7776ed3d06869c3051
Mark Harmstone <mark@harmstone.com>
2021-04-17 17:25:00 +0100

    add archive functions to pdb

--- bfd/pdb.c
+++ bfd/pdb.c
@@ -6,6 +6,27 @@
 
 static bfd_cleanup pdb_check_format (bfd *abfd);
 
+static bfd_boolean pdb_archive_slurp_armap (bfd *abfd);
+static bfd_boolean pdb_archive_slurp_extended_name_table (bfd *abfd);
+static bfd_boolean pdb_archive_construct_extended_name_table (bfd *abfd ATTRIBUTE_UNUSED,
+							      char **tabloc ATTRIBUTE_UNUSED,
+							      bfd_size_type *tablen ATTRIBUTE_UNUSED,
+							      const char **name ATTRIBUTE_UNUSED);
+static void pdb_archive_truncate_arname (bfd *abfd ATTRIBUTE_UNUSED,
+					 const char *pathname ATTRIBUTE_UNUSED,
+					 char *arhdr ATTRIBUTE_UNUSED);
+static bfd_boolean pdb_archive_write_armap(bfd *arch ATTRIBUTE_UNUSED,
+					   unsigned int elength ATTRIBUTE_UNUSED,
+					   struct orl *map ATTRIBUTE_UNUSED,
+					   unsigned int orl_count ATTRIBUTE_UNUSED,
+					   int stridx ATTRIBUTE_UNUSED);
+static void *pdb_archive_read_ar_hdr (bfd *abfd ATTRIBUTE_UNUSED);
+static bfd_boolean pdb_archive_write_ar_hdr (bfd *archive, bfd *abfd ATTRIBUTE_UNUSED);
+static bfd *pdb_archive_openr_next_archived_file (bfd *archive, bfd *last_file ATTRIBUTE_UNUSED);
+static bfd *pdb_archive_get_elt_at_index (bfd *abfd, symindex sym_index ATTRIBUTE_UNUSED);
+static int pdb_archive_generic_stat_arch_elt (bfd *abfd ATTRIBUTE_UNUSED, struct stat *buf ATTRIBUTE_UNUSED);
+static bfd_boolean pdb_archive_update_armap_timestamp (bfd *arch ATTRIBUTE_UNUSED);
+
 const bfd_target pdb_vec =
 {
   "pdb",
@@ -28,8 +49,8 @@ const bfd_target pdb_vec =
 
   {				/* bfd_check_format */
     _bfd_dummy_target,
-    pdb_check_format,
     _bfd_dummy_target,
+    pdb_check_format,
     _bfd_dummy_target
   },
   {				/* bfd_create_object */
@@ -48,7 +69,7 @@ const bfd_target pdb_vec =
   BFD_JUMP_TABLE_GENERIC (_bfd_generic),
   BFD_JUMP_TABLE_COPY (_bfd_generic),
   BFD_JUMP_TABLE_CORE (_bfd_nocore),
-  BFD_JUMP_TABLE_ARCHIVE (_bfd_noarchive),
+  BFD_JUMP_TABLE_ARCHIVE (pdb_archive),
   BFD_JUMP_TABLE_SYMBOLS (_bfd_nosymbols),
   BFD_JUMP_TABLE_RELOCS (_bfd_norelocs),
   BFD_JUMP_TABLE_WRITE (_bfd_generic),
@@ -82,4 +103,77 @@ pdb_check_format (bfd *abfd)
   // FIXME - load data etc.
 
   return _bfd_no_cleanup;
+}
+
+static bfd_boolean
+pdb_archive_slurp_armap (bfd *abfd ATTRIBUTE_UNUSED)
+{
+  return FALSE;
+}
+
+static bfd_boolean
+pdb_archive_slurp_extended_name_table (bfd *abfd ATTRIBUTE_UNUSED)
+{
+  return FALSE;
+}
+
+static bfd_boolean
+pdb_archive_construct_extended_name_table (bfd *abfd ATTRIBUTE_UNUSED, char **tabloc ATTRIBUTE_UNUSED,
+					   bfd_size_type *tablen ATTRIBUTE_UNUSED,
+					   const char **name ATTRIBUTE_UNUSED)
+{
+  return FALSE;
+}
+
+static void
+pdb_archive_truncate_arname (bfd *abfd ATTRIBUTE_UNUSED,
+			     const char *pathname ATTRIBUTE_UNUSED,
+			     char *arhdr ATTRIBUTE_UNUSED)
+{
+}
+
+static bfd_boolean
+pdb_archive_write_armap(bfd *arch ATTRIBUTE_UNUSED,
+			unsigned int elength ATTRIBUTE_UNUSED,
+			struct orl *map ATTRIBUTE_UNUSED,
+			unsigned int orl_count ATTRIBUTE_UNUSED,
+			int stridx ATTRIBUTE_UNUSED)
+{
+  return TRUE;
+}
+
+static void *
+pdb_archive_read_ar_hdr (bfd *abfd ATTRIBUTE_UNUSED)
+{
+  return NULL;
+}
+
+static bfd_boolean
+pdb_archive_write_ar_hdr (bfd *archive, bfd *abfd ATTRIBUTE_UNUSED)
+{
+  return _bfd_bool_bfd_false_error (archive);
+}
+
+static bfd *
+pdb_archive_openr_next_archived_file (bfd *archive, bfd *last_file ATTRIBUTE_UNUSED)
+{
+  return (bfd *) _bfd_ptr_bfd_null_error (archive);
+}
+
+static bfd *
+pdb_archive_get_elt_at_index (bfd *abfd, symindex sym_index ATTRIBUTE_UNUSED)
+{
+  return (bfd *) _bfd_ptr_bfd_null_error (abfd);
+}
+
+static int
+pdb_archive_generic_stat_arch_elt (bfd *abfd ATTRIBUTE_UNUSED, struct stat *buf ATTRIBUTE_UNUSED)
+{
+  return -1;
+}
+
+static bfd_boolean
+pdb_archive_update_armap_timestamp (bfd *arch ATTRIBUTE_UNUSED)
+{
+  return FALSE;
 }
